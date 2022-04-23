c80e4da683f899f240da24587cad4a821905c216
BenWoolford30 <benwoolford30@gmail.com>
2022-04-23 12:42:18 +0100

    console input in C#
    Project for console input in C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿#region ""
+using System;
+
+namespace LS_4_Input
+{
+    class Program
+    {
+        #endregion
+        /*
+            Welcome to Input- You may have noticed the Console.ReadLine() statement previously?
+
+            We can use that to take text from the user and assign it to a string like so!
+
+            
+        */
+
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("I will output what you type!");
+
+            string userinput = Console.ReadLine();
+
+            Console.WriteLine(userinput);
+
+            Console.ReadLine();
+
+
+        }
+#region "" 
+    }
+}
+#endregion
